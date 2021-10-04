607ab1bdeffaa405092a729b83e2f9f2e4d072b8
Eduardo Balzan <eduardo.balzan@edu.pucrs.br>
2021-10-04 18:52:18 -0300

    Verificação das maneiras de criar Voos

--- MyFlight_base/src/pucrs/myflight/consoleApp/App.java
+++ MyFlight_base/src/pucrs/myflight/consoleApp/App.java
@@ -12,7 +12,7 @@ import java.time.Duration;
 
 public class App {
 	public static void main(String[] args) {
-		// 1º voo
+		// 1º voo com dois tipos de criação de voo
 		CiaAerea cia1 = new CiaAerea("01", "Azul");
 		Aeronave aer1 = new Aeronave("001", "A380");
 		Geo g1 = new Geo(-29.99, -51.17);
@@ -22,6 +22,7 @@ public class App {
 		Rota r1 = new Rota(cia1, orig1, dest1, aer1);
 		LocalDateTime horaLocal1 = LocalDateTime.of(2016, 8, 12, 12, 0);
 		Duration duracao = Duration.ofMinutes(120);
-		Voo v1 = new Voo(r1, horaLocal1, duracao);	
+		Voo v1a = new Voo(r1, horaLocal1, duracao);	
+		Voo v1b = new Voo(r1);
 	}
 }
