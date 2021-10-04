091c8ccc1011829e37746049603fdfc8b1c8e525
Eduardo Balzan <eduardo.balzan@edu.pucrs.br>
2021-10-04 18:43:53 -0300

    Criação de Voos

--- MyFlight_base/src/pucrs/myflight/consoleApp/App.java
+++ MyFlight_base/src/pucrs/myflight/consoleApp/App.java
@@ -1,9 +1,27 @@
 package pucrs.myflight.consoleApp;
 
+import pucrs.myflight.modelo.Aeronave;
+import pucrs.myflight.modelo.Aeroporto;
+import pucrs.myflight.modelo.CiaAerea;
+import pucrs.myflight.modelo.Geo;
+import pucrs.myflight.modelo.Rota;
+import pucrs.myflight.modelo.Voo;
+
+import java.time.LocalDateTime;
+import java.time.Duration;
+
 public class App {
-
 	public static void main(String[] args) {
-		System.out.println("\nMyFlight project...");
-
+		// 1º voo
+		CiaAerea cia1 = new CiaAerea("01", "Azul");
+		Aeronave aer1 = new Aeronave("001", "A380");
+		Geo g1 = new Geo(-29.99, -51.17);
+		Geo g2 = new Geo(-23.43, -46.47);
+		Aeroporto orig1 = new Aeroporto("123", "Salgado Filho", g1);
+		Aeroporto dest1 = new Aeroporto("456", "Guarulhos", g2);
+		Rota r1 = new Rota(cia1, orig1, dest1, aer1);
+		LocalDateTime horaLocal1 = LocalDateTime.of(2016, 8, 12, 12, 0);
+		Duration duracao = Duration.ofMinutes(120);
+		Voo v1 = new Voo(r1, horaLocal1, duracao);	
 	}
 }
