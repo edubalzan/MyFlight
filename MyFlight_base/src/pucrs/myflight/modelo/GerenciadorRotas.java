1349ea6154dbe095d75cb9f6cba11714c3d154d3
Eduardo Balzan <eduardo.balzan@edu.pucrs.br>
2021-11-11 22:09:31 -0300

    Commit final

--- MyFlight_base/src/pucrs/myflight/modelo/GerenciadorRotas.java
+++ MyFlight_base/src/pucrs/myflight/modelo/GerenciadorRotas.java
@@ -6,12 +6,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-// import java.io.BufferedReader;
-// import java.io.IOException;
-// import java.nio.charset.Charset;
-// import java.nio.file.Files;
-// import java.nio.file.Path;
-// import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 
